44f91c7528d290d1e256cfc8efcaf5ba807f4a93
tranquillo <rob.tranquillo@gmx.de>
2015-01-26 07:23:48 +0100

    erste Version. Arbeitet mit einer gefüllten MongoDB zusammen

---
+++ server (static).js
@@ -0,0 +1,4 @@
+## Statischer Ansatz
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080);
---
+++ server.js
@@ -0,0 +1,39 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(8080);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+/*
+ * socket handling
+ */
+io.on('connection', function(socket) {
+    //etwas vom client empfangen
+    socket.on('mongodbsearch', function(data) {
+        if( data['userdata'] != '' )
+
+            //search in different collections with submudule risdd{} 
+            var risdd = require("./lib/risdd.js");
+            risdd.query({ search : data['userdata'] , collection : "sessions" }, function(requestResult){
+                socket.emit('serverdata', requestResult );
+            });
+    });
+});
+
+
+
+
+
