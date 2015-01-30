386d379cecd067644b580f69d201df83908898c4
tranquillo <rob.tranquillo@gmx.de>
2015-01-30 08:51:15 +0100

    Setup utf8 compatibility. Start showing some selects to getting a overview

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
-var fs = require('fs');
+var app = require("http").createServer(handler);
+var io = require("socket.io")(app);
+var fs = require("fs");
 
 app.listen(8080);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
+  fs.readFile(__dirname + "/index.html",
   function (err, data) {
     if (err) {
       res.writeHead(500);
-      return res.end('Error loading index.html');
+      return res.end("Error loading index.html");
     }
 
     res.writeHead(200);
@@ -20,13 +20,21 @@ function handler (req, res) {
 /*
  * socket handling
  */
-io.on('connection', function(socket) {
-    //etwas vom client empfangen
-    socket.on('mongodbsearch', function(data) {
-        if( data['userdata'] != '' )
+io.on("connection", function(socket) {
 
-            //search in different collections with submudule risdd{} 
-            var risdd = require("./lib/risdd.js");
+    socket.on("mongodbsearch", function(data) {
+        //load database handling module
+        var risdd = require("./lib/risdd.js");
+
+        // fill up overviews
+        if( data["overview"] !== undefined ) {
+            risdd.query({search : "", collection : "person", key : "title"},
+                        function(requestResult){ socket.emit("serverdata", { type : "ov_persons", data : requestResult })})
+        }
+
+
+        // handle user search
+        if( data["userdata"] !== undefined ){
             risdd.query({ search : data["userdata"] , collection : "sessions" , key : "description" },
                         function(requestResult){ socket.emit("serverdata", { type : "sessions" , data : requestResult });
             });
@@ -34,11 +42,10 @@ io.on('connection', function(socket) {
             risdd.query({ search : data["userdata"] , collection : "person" , key : "title" },
                         function(requestResult){ socket.emit("serverdata", { type : "person", data : requestResult });
             });
-            
+        }
     });
+    
 });
-
-
 
 
 
