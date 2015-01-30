cd315a240eb24ba7430ba9037b00a5e723bb3cd3
tranquillo <rob.tranquillo@gmx.de>
2015-01-30 08:54:58 +0100

    change all " to \'

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-var app = require("http").createServer(handler);
-var io = require("socket.io")(app);
-var fs = require("fs");
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
 
 app.listen(8080);
 
 function handler (req, res) {
-  fs.readFile(__dirname + "/index.html",
+  fs.readFile(__dirname + '/index.html',
   function (err, data) {
     if (err) {
       res.writeHead(500);
-      return res.end("Error loading index.html");
+      return res.end('Error loading index.html');
     }
 
     res.writeHead(200);
@@ -20,27 +20,27 @@ function handler (req, res) {
 /*
  * socket handling
  */
-io.on("connection", function(socket) {
+io.on('connection', function(socket) {
 
-    socket.on("mongodbsearch", function(data) {
+    socket.on('mongodbsearch', function(data) {
         //load database handling module
-        var risdd = require("./lib/risdd.js");
+        var risdd = require('./lib/risdd.js');
 
         // fill up overviews
-        if( data["overview"] !== undefined ) {
-            risdd.query({search : "", collection : "person", key : "title"},
-                        function(requestResult){ socket.emit("serverdata", { type : "ov_persons", data : requestResult })})
+        if( data['overview'] !== undefined ) {
+            risdd.query({search : '', collection : 'person', key : 'title'},
+                        function(requestResult){ socket.emit('serverdata', { type : 'ov_persons', data : requestResult })})
         }
 
 
         // handle user search
-        if( data["userdata"] !== undefined ){
-            risdd.query({ search : data["userdata"] , collection : "sessions" , key : "description" },
-                        function(requestResult){ socket.emit("serverdata", { type : "sessions" , data : requestResult });
+        if( data['userdata'] !== undefined ){
+            risdd.query({ search : data['userdata'] , collection : 'sessions' , key : 'description' },
+                        function(requestResult){ socket.emit('serverdata', { type : 'sessions' , data : requestResult });
             });
 
-            risdd.query({ search : data["userdata"] , collection : "person" , key : "title" },
-                        function(requestResult){ socket.emit("serverdata", { type : "person", data : requestResult });
+            risdd.query({ search : data['userdata'] , collection : 'person' , key : 'title' },
+                        function(requestResult){ socket.emit('serverdata', { type : 'person', data : requestResult });
             });
         }
     });
